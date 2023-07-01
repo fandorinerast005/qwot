edd6b5c4aace47f795503e1784b9b67e7b94aa4c
Glitch (glitch-hello-website) <none>
2023-07-01 14:29:22 +0000

    🔮👛 Checkpoint ./script.js:59527422/8626 ./index.html:59527422/4393 ./style.css:59527422/51

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
-      const navLinks = document.querySelectorAll('nav ul li a');
-      const body = document.body;
-      const header = document.querySelector('header');
-      const contactSection = document.getElementById('section4');
-      const sections = document.querySelectorAll('section');
-      const footer = document.querySelector('footer');
-      const title = document.getElementById('title');
-      const aboutText = document.getElementById('about-text');
+// script.js
+const navLinks = document.querySelectorAll('nav ul li a');
+const body = document.body;
+const header = document.querySelector('header');
+const contactSection = document.getElementById('section4');
+const aboutText = document.getElementById('about-text');
+const sections = document.querySelectorAll('section');
+const footer = document.querySelector('footer');
 
-      navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-          const randomColor = getRandomColor();
-          body.style.backgroundColor = randomColor;
-          setContrastText(randomColor);
-        });
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    const randomColor = getRandomColor();
+    body.style.backgroundColor = randomColor;
+    setContrastText(randomColor);
+  });
+});
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setContrastText(backgroundColor) {
+  const brightness = getBrightness(backgroundColor);
+  const textColor = brightness > 127.5 ? '#000' : '#FFF';
+  body.style.color = textColor;
+  header.style.color = textColor;
+  contactSection.style.color = textColor;
+  aboutText.style.color = textColor;
+  navLinks.forEach(link => {
+    link.style.color = textColor;
+  });
+  sections.forEach(section => {
+    if (section !== contactSection) {
+      const sectionText = section.querySelectorAll(':not(h2)');
+      sectionText.forEach(text => {
+        text.style.color = textColor;
       });
+    }
+  });
+  footer.style.color = textColor;
+}
 
-      function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-      }
+function getBrightness(color) {
+  const rgb = hexToRgb(color);
+  const brightness = (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
+  return brightness;
+}
 
-      function setContrastText(backgroundColor) {
-        const brightness = getBrightness(backgroundColor);
-        const textColor = brightness > 127.5 ? '#000' : '#FFF';
-        body.style.color = textColor;
-        header.style.color = textColor;
-        contactSection.style.color = textColor;
-        title.style.color = textColor;
-        aboutText.style.color = textColor;
-        navLinks.forEach(link => {
-          link.style.color = textColor;
-        });
-        sections.forEach(section => {
-          if (section !== contactSection) {
-            const sectionText = section.querySelectorAll(':not(h2)');
-            sectionText.forEach(text => {
-              text.style.color = textColor;
-            });
-          }
-        });
-        footer.style.color = textColor;
+function hexToRgb(hex) {
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+    return r + r + g + g + b + b;
+  });
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
       }
-
-      function getBrightness(color) {
-        const rgb = hexToRgb(color);
-        const brightness = (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
-        return brightness;
-      }
-
-      function hexToRgb(hex) {
-        const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-        hex = hex.replace(shorthandRegex, (m, r, g, b) => {
-          return r + r + g + g + b + b;
-        });
-        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result
-          ? {
-              r: parseInt(result[1], 16),
-              g: parseInt(result[2], 16),
-              b: parseInt(result[3], 16),
-            }
-          : null;
-      }
+    : null;
+}
